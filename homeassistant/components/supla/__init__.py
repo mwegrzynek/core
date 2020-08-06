6deddd43b9174af7eeab8c707c538b78f26947e1
Michał Węgrzynek <michal.wegrzynek@malloc.com.pl>
2020-08-06 17:47:41 +0200

    Update interval calculation

--- homeassistant/components/supla/__init__.py
+++ homeassistant/components/supla/__init__.py
@@ -1,7 +1,8 @@
 """Support for Supla devices."""
-import logging
-from typing import Optional
 from datetime import timedelta
+import logging
+import math
+from typing import Optional
 
 from pysupla import SuplaAPI
 import voluptuous as vol
@@ -11,7 +12,6 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import load_platform
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
-
 
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = "supla"
@@ -27,12 +27,13 @@ SUPLA_FUNCTION_HA_CMP_MAP = {
 SUPLA_FUNCTION_NONE = "NONE"
 SUPLA_CHANNELS = "supla_channels"
 SUPLA_SERVERS = "supla_servers"
+SUPLA_API_CALL_LIMIT = 950
 
 SERVER_CONFIG = vol.Schema(
     {
         vol.Required(CONF_SERVER): cv.string,
         vol.Required(CONF_ACCESS_TOKEN): cv.string,
-        vol.Optional(CONF_SCAN_INTERVAL): cv.time_period
+        vol.Optional(CONF_SCAN_INTERVAL): cv.time_period,
     }
 )
 
@@ -47,9 +48,9 @@ CONFIG_SCHEMA = vol.Schema(
 
 SCAN_INTERVAL = timedelta(seconds=10)
 
+
 def setup(hass, base_config):
     """Set up the Supla component."""
-
     server_confs = base_config[DOMAIN][CONF_SERVERS]
 
     hass.data[SUPLA_SERVERS] = {}
@@ -61,8 +62,9 @@ def setup(hass, base_config):
 
         server = SuplaAPI(server_address, server_conf[CONF_ACCESS_TOKEN])
 
-        # Set update interval on server to be used by channel devices
-        server.update_interval = server_conf.get(CONF_SCAN_INTERVAL, SCAN_INTERVAL)
+        # Set update interval on server to be used by channel devices.
+        # If no interval is configured, calculate it during device discovery.
+        server.update_interval = server_conf.get(CONF_SCAN_INTERVAL)
 
         # Test connection
         try:
@@ -97,7 +99,29 @@ def discover_devices(hass, hass_config):
 
     for server_name, server in hass.data[SUPLA_SERVERS].items():
 
-        for channel in server.get_channels(include=["iodevice", "connected", "state"]):
+        channels = server.get_channels(include=["iodevice", "connected", "state"])
+
+        update_interval = None
+        if server.update_interval is None:
+            # Calculate minimum workable update interval.
+            # The current API call limit on public Supla servers is 1000 calls per hour
+            # (module limit is set to 950 calls to allow some room for calculation differences).
+            update_interval = 3600 * len(channels) / SUPLA_API_CALL_LIMIT
+        else:
+            update_interval = server.update_interval.total_seconds()
+
+        # Round the update_interval to module level SCAN_INTERVAL.
+        scan_interval_seconds = SCAN_INTERVAL.total_seconds()
+        update_interval = (
+            math.ceil(update_interval / scan_interval_seconds) * scan_interval_seconds
+        )
+        _LOGGER.debug(
+            "Current update interval for server %s is: %.0f seconds",
+            server_name,
+            update_interval,
+        )
+
+        for channel in channels:
             channel_function = channel["function"]["name"]
             if channel_function == SUPLA_FUNCTION_NONE:
                 _LOGGER.debug(
@@ -119,8 +143,7 @@ def discover_devices(hass, hass_config):
 
             channel["server_name"] = server_name
 
-            # Pass configured update interval from server configuration
-            channel["update_interval"] = server.update_interval.total_seconds()
+            channel["update_interval"] = update_interval
 
             component_configs.setdefault(component_name, []).append(channel)
 
@@ -160,7 +183,7 @@ class SuplaChannel(Entity):
     @property
     def available(self) -> bool:
         """Return True if entity is available."""
-        _LOGGER.debug('Channel data: %s', self.channel_data)
+        _LOGGER.debug("Channel data: %s", self.channel_data)
 
         if self.channel_data is None:
             return False
@@ -186,7 +209,7 @@ class SuplaChannel(Entity):
 
     def _update(self):
         """Call to update state."""
-        _LOGGER.debug('Calling update on Supla channel: %d', self.channel_data["id"])
+        _LOGGER.debug("Calling update on Supla channel: %d", self.channel_data["id"])
         self.channel_data = self.server.get_channel(
             self.channel_data["id"], include=["connected", "state"]
         )
